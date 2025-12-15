dea0e1d97a34f7ad78ea46ff6d3c5324975e76aa
Mariano Belgrano <mbelgrano@gmail.com>
2025-12-15 11:03:58 -0300

    applied css gap

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import { Container } from 'react-bootstrap';
 const About = () => {
   return (
     <>
-      <Container>
-        <h2>Pagina About</h2>
+      <Container className="py-4">
+        <h2 className="mb-3">Pagina About</h2>
       </Container>
     </>
   );
