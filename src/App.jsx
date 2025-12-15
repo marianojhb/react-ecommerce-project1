fab1e5e49ca21824b69b60aa1ba4b6de8b36aed3
Mariano Belgrano <mbelgrano@gmail.com>
2025-12-14 21:59:47 -0300

    Agregadas imagenes a los productos

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,21 @@ function App() {
       desc: 'Medias',
       stock: 10,
       precio: 150,
+      img: 'https://fastly.picsum.photos/id/320/600/800.jpg?hmac=-6i2-GinGAdR7I9n1jH45aHwj-XzTU6jOjuGH57vgbE',
     },
     {
       id: 2,
       desc: 'Zapatos',
       stock: 5,
       precio: 350,
+      img: 'https://fastly.picsum.photos/id/773/600/800.jpg?hmac=LnnX7dE5wbZ78GT9cipPYULuuqMvnnqvasYErcQYxlE',
     },
     {
       id: 3,
       desc: 'Pantalones',
       stock: 30,
       precio: 500,
+      img: 'https://fastly.picsum.photos/id/439/600/800.jpg?hmac=JeTnMG7yR2zviIzqoYGpsxdpzwSxMCJZ8d-6cachhFQ',
     },
   ];
 
