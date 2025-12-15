b2d852db2a601c5e309cf8d040354c568218c88b
Mariano Belgrano <mbelgrano@gmail.com>
2025-12-15 11:04:43 -0300

    mockapi implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import About from './pages/About';
 import Checkout from './pages/Checkout';
@@ -8,31 +8,16 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
 
-  const listadoDeProductos = [
-    {
-      id: 1,
-      desc: 'Medias',
-      stock: 10,
-      precio: 150,
-      img: 'https://fastly.picsum.photos/id/320/600/800.jpg?hmac=-6i2-GinGAdR7I9n1jH45aHwj-XzTU6jOjuGH57vgbE',
-    },
-    {
-      id: 2,
-      desc: 'Zapatos',
-      stock: 5,
-      precio: 350,
-      img: 'https://fastly.picsum.photos/id/773/600/800.jpg?hmac=LnnX7dE5wbZ78GT9cipPYULuuqMvnnqvasYErcQYxlE',
-    },
-    {
-      id: 3,
-      desc: 'Pantalones',
-      stock: 30,
-      precio: 500,
-      img: 'https://fastly.picsum.photos/id/439/600/800.jpg?hmac=JeTnMG7yR2zviIzqoYGpsxdpzwSxMCJZ8d-6cachhFQ',
-    },
-  ];
+function App() {
+  useEffect(() => {
+    fetch('https://68ebcbd276b3362414ceb1d3.mockapi.io/api/v1/products')
+      .then((res) => res.json())
+      .then((json) => setListadoDeProductos(json));
+  }, []);
+
+  const [cargando, setCargando] = useState(false);
+  const [listadoDeProductos, setListadoDeProductos] = useState([]);
 
   const [cart, setCart] = useState([]);
 
@@ -42,7 +27,7 @@ function App() {
         <div>
           <Navegacion cart={cart} />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home listadoDeProductos={listadoDeProductos} cart={cart} setCart={setCart} />} />
             <Route path="/about" element={<About />} />
             <Route path="/checkout" element={<Checkout cart={cart} setCart={setCart} />} />
             <Route path="/contact" element={<Contact />} />
@@ -53,6 +38,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+
     </>
   );
 }
